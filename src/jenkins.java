51a8bcb268f8d495728d6486caac0918d25df6ee
Dileepkumargit <thupatidileep@gmail.com>
2020-09-21 12:42:18 +0530

    secong commit

--- src/jenkins.java
+++ src/jenkins.java
@@ -1,15 +1,14 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class jenkins {
 
 	public static void main(String[] args) {
+		System.out.println("exectue on cmd");
 		
-		
-		System.setProperty("webdriver.chrome.driver","D:\\selenium latest\\chromedriver_win32\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://www.google.com");
+//		System.setProperty("webdriver.chrome.driver","D:\\selenium latest\\chromedriver_win32\\chromedriver.exe");
+//		WebDriver driver = new ChromeDriver();
+//		driver.manage().window().maximize();
+//		driver.get("https://www.google.com");
 		
 
 	}
